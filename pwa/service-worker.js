ecece64d053ad33e6de277249783a1388da3eddc
akakou <mitani@beeworks.co.jp>
2020-04-09 17:07:19 +0900

    [modify] オフラインでも表示できるよう処理を追加

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -8,3 +8,32 @@ self.addEventListener('activate', function(e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
+
+// 以下：https://qiita.com/poster-keisuke/items/6651140fa20c7aa18474
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+		'/pwa/'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
